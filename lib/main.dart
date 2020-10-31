55b271d5f2a30325e60b9de97cef33a2aa9291db
sayed mohamed <sayedmohamedscu@gmail.com>
2020-10-31 18:19:44 +0200

    updates

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:wifi_flutter/wifi_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> _platformVersion = [];
+  List<Widget> a=[];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: ListView.builder(
+            itemBuilder: (context, i) => _platformVersion[i],
+            itemCount: _platformVersion.length,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final noPermissions = await WifiFlutter.promptPermissions();
+            if (noPermissions) {
+              return;
+            }
+            final networks = await WifiFlutter.wifiNetworks;
+            setState(() {
+              _platformVersion = networks.map((network) => Text(
+                  "Ssid ${network.ssid} - Strength ${network.rssi} ")).toList();
+                 // .toList();
+              a.addAll(_platformVersion);
+              print(a);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}

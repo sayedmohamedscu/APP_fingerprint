cc9ce326f381e935c682b743ac784257282dbe95
Abdo-Sameh <abdelrahman.sameh.1420@gmail.com>
2020-11-06 23:58:29 +0200

    Beta01

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,14 @@
+// import 'dart:html';
 import 'package:flutter/material.dart';
-import 'package:wifi_flutter/wifi_flutter.dart';
-import 'dart:io';
-import 'dart:convert';
-import 'dart:async' show Future;
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:path_provider/path_provider.dart'; //add path provider dart plugin on pubspec.yaml file
+import 'screen2.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Screen1(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List _platformVersion = [];
-  List _platformVersion2 = [];
-  List results = [];
-  List ap1 = [];
-  List ap2 = [];
-  List ap3 = [];
-  var i = 0;
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  final myController = TextEditingController();
-
+class Screen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -44,181 +21,41 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
       body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
-            padding: const EdgeInsets.all(50),
+            padding: const EdgeInsets.all(20),
             child: Text(
-              'Enter Your Area Name !',
+              'Welcome !',
               style: TextStyle(
-                inherit: true,
                 fontFamily: 'BebasNeue',
-                fontSize: 30,
+                fontSize: 80,
                 color: Colors.white,
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 12),
-            child: TextField(
-              maxLengthEnforced: true,
-              decoration: InputDecoration(
-                hintText: 'Example: Area01',
-                hintStyle: TextStyle(fontSize: 13),
-                icon: Icon(
-                  Icons.add_location_rounded,
-                  color: Colors.white,
-                  size: 35,
-                ),
-                prefixIcon: Icon(
-                  Icons.keyboard,
-                  // color: Colors.teal,
-                ),
-                prefixText: 'Area: ',
-                prefixStyle: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-                filled: true,
-                fillColor: Colors.grey[300],
-                labelText: 'Area Name',
-                labelStyle: TextStyle(color: Colors.black, fontSize: 18),
-                suffix: Container(
-                  width: 5,
-                  height: 5,
+            padding: const EdgeInsets.all(20),
+            child: Center(
+              child: RaisedButton(
                   color: Colors.teal,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.all(Radius.circular(30)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blue),
-                  borderRadius: BorderRadius.all(Radius.circular(30)),
-                ),
-              ),
-              // autofocus: true,
-              maxLength: 10,
-              cursorHeight: 30,
-              cursorColor: Colors.teal,
-              controller: myController,
-            ),
-          ),
-          FlatButton(
-              // minWidth: 150,
-              height: 50,
-              color: Colors.teal,
-              child: Text(
-                'Scan',
-                style: TextStyle(
-                  inherit: true,
-                  // fontFamily: 'BebasNeue',
-                  fontSize: 22,
-                  color: Colors.white,
-                ),
-              ),
-              // child: Icon(
-              //   Icons.search,
-              //   size: 50,
-              //   color: Colors.white,
-              // ),
-              onPressed: () async {
-                print((myController.text));
-                //
-                ap1 = [0];
-                ap2 = [0];
-                ap3 = [0];
-                results = [0];
-                //await Permission.contacts.request()
-
-// code of read or write file in external storage (SD card)
-
-                final noPermissions = await WifiFlutter.promptPermissions();
-                if (noPermissions) {
-                  return;
-                }
-                for (i = 0; i <= 50; i++) {
-                  await Future.delayed(Duration(milliseconds: 100));
-
-                  final networks = await WifiFlutter.wifiNetworks;
-                  setState(() {
-                    // Text('Scanning...');
-                    _platformVersion = networks.map((network) {
-                      return network.ssid;
-                    }).toList();
-                    _platformVersion2 = networks.map((network) {
-                      return network.rssi;
-                    }).toList();
-
-                    //a.addAll(_platformVersion);
-                    print(_platformVersion);
-
-                    // select the name
-                    for (var eleInd = 0;
-                        eleInd < _platformVersion.length;
-                        eleInd++) {
-                      //AP1
-                      //print(_platformVersion[ele_ind].data);
-                      if (_platformVersion[eleInd] == 'tedata') {
-                        print(_platformVersion2[eleInd]);
-                        ap1.add(_platformVersion2[eleInd]);
-                      }
-                      //AP2
-                      if (_platformVersion[eleInd] == 'Orange-gogo gamal') {
-                        print(_platformVersion2[eleInd]);
-                        ap2.add(_platformVersion2[eleInd]);
-                      }
-
-                      //AP3
-                      if (_platformVersion[eleInd] == 'Basma') {
-                        print(_platformVersion2[eleInd]);
-                        ap3.add(_platformVersion2[eleInd]);
-                      }
-                    }
-
-                    print('ap');
-                    print(ap1);
-                    print(_platformVersion.length);
-                    print(i);
-                    //print(ap2);
-                    //print(ap3);
-                  });
-                }
-
-                //var json = jsonEncode(ap1, toEncodable: (e) => e.toJsonAttr());
-                //String rawJson = jsonEncode(ap1);
-                //print(rawJson);
-                //File('/data/user/0/com.example.flutter_appf/assets/sayed.txt').writeAsString('$ap1');
-
-                //Future<String> loadAsset() async {
-                //return await rootBundle.loadString('assets/sayed.txt');
-                //  }
-
-                print((myController.text));
-                //Directory tempDir = await getExternalStorageDirectory();
-                Directory tempDir = await getApplicationDocumentsDirectory();
-
-                String tempPath = tempDir.path;
-                print(tempPath);
-                results.addAll([ap1, ap2, ap3]);
-
-                File f1 = new File('s1.txt');
-                File file = new File(tempPath + "/" + '.txt');
-                file.createSync();
-                var nameOfArea = myController.text;
-                File(tempPath + "/" + '$nameOfArea' + '.txt')
-                    .writeAsString('$results');
-                String contents = await file.readAsString();
-                print(contents);
-              }),
-          Padding(
-            padding: const EdgeInsets.only(top: 75),
-            child: Text(
-              '$i Samples Has Been Scanned!',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Start\nScanning',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Screen2()),
+                    );
+                  }),
             ),
           )
         ],
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_appf/main.dart';
+import 'package:flutter_appf/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_appf/main.dart';
@@ -13,7 +15,7 @@ import 'package:flutter_appf/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Screen1());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
